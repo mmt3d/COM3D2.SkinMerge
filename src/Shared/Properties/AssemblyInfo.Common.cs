da2e53bfec5c7c2e8eefb6652e25191a8a4682dc
mmt3d <78088314+mmt3d@users.noreply.github.com>
2025-12-29 00:19:52 +0900

    release: v1.0.4.0

--- src/Shared/Properties/AssemblyInfo.Common.cs
+++ src/Shared/Properties/AssemblyInfo.Common.cs
@@ -5,7 +5,7 @@ namespace COM3D2.SkinMerge
     {
         public const string PluginTarget = "COM3D2.";
         public const string PluginName = "SkinMerge";
-        public const string PluginVersion = "1.0.3.0";
+        public const string PluginVersion = "1.0.4.0";
         public const string PluginCopyright = "Copyright © mmt3d 2025";
         public const string PluginFullName = PluginTarget + PluginName;
         public const string PluginTitleName = PluginName + " " + PluginVersion;
